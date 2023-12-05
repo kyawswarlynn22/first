cb757d092b3e8a5b2c309e19501acb5aa94d5379
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-12-05 15:30:11 +0630

    update

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log(req.url,req.method,req.headers);
-   
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>First Response</title><head>');
+    res.write('<body><h1>My First Response Page</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
